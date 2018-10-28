d584b8bcccebb87eac07f5abc068e3f7dae40f68
CoolCoolBo <512167784@qq.com>
2018-10-28 22:35:41 +0800

    重构后的代码

--- 2_classify/classify_problem2.py
+++ 2_classify/classify_problem2.py
@@ -1,6 +1,11 @@
 import os
 import shutil
 
+"""
+将problem2_files里面各个文件里面的东西整理并分为image和document两类
+
+"""
+
 image_folder_list = ['jpg', 'png', 'gif']
 document_folder_list = ['doc', 'docx', 'md', 'ppt']
 path = './problem2_files'
@@ -10,19 +15,18 @@ document_folder_path = './problem2_files/documents'
 image = os.mkdir(image_folder_path)
 document = os.mkdir(document_folder_path)
 
+def classify(folder, folder_path):
+	folder = os.path.join(path, folder)
+	files = os.listdir(folder)
+	for file in files:
+		file = os.path.join(folder, file)
+		shutil.move(file, folder_path)
+		
 for folder in folders:
 
 	if folder in image_folder_list:
-		folder = os.path.join(path, folder)
-		files = os.listdir(folder)
-		for file in files:
-			file = os.path.join(folder, file)
-			shutil.move(file, image_folder_path)
+		classify(folder, image_folder_path)
 
 	if folder in document_folder_list:
-		folder = os.path.join(path, folder)
-		files = os.listdir(folder)
-		for file in files:
-			file = os.path.join(folder, file)
-			shutil.move(file, document_folder_path)
+		classify(folder, document_folder_path)
 
