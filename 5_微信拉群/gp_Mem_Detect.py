0f4dfe508d13e88392e157f766abc2b9c53e048b
CoolCoolBo <512167784@qq.com>
2018-11-02 15:49:16 +0800

    检测群成员加入与退出，并将消息发送给指定人

--- 5_微信拉群/gp_Mem_Detect.py
+++ 5_微信拉群/gp_Mem_Detect.py
@@ -1,43 +1,46 @@
-import wxpy
+from wxpy import *
+import time
 
-# members = []
-# for member in group:
-# 	members.append(member)
-for member in members:
-	result = group.search(member.name)
-	if result == None:
-
-def isIn(group):
+def isIn(members_before, members_now):
 	for each in members_now:
 		if each not in members_before:
 			change_members.append(each.name)
 			return change_members
 
-def isOut(group):
+def isOut(members_before, members_now):
 	for each in members_before:
 		if each not in members_now:
 			change_members.append(each.name)
 			return change_members
 
-def sendMsg(change_members):
+def sendOutMsg(change_members, bot):
 	for member in change_members:
-		msg = '{}已经退出群聊'.format(member.name)
+		msg = '{}退出了群聊'.format(member)
 		print(msg)
+		myself = bot.friends().search('胡松江')[0]
+		myself.send(msg)
+
+def sendInMsg(change_members, bot):
+	for member in change_members:
+		msg = '{}加入了群聊'.format(member)
+		print(msg)
+		myself = bot.friends().search('胡松江')[0]
+		myself.send(msg)
 
 bot = Bot()
-group = bot.groups().search("小铃铛网咖活动交流群")[0]
+group = bot.groups().search("测试群")[0]
 members_before = group.members
 while True:
-	# members_before = group.members
 	change_members = []
-	time.sleep(60)
+	time.sleep(10)
+	group = bot.groups().search("测试群")[0]
 	members_now = group.members
 	if len(members_now) < len(members_before):
-		change_members = isOut(group)
-		sendMsg(change_members)
+		change_members = isOut(members_before, members_now)
+		sendOutMsg(change_members, bot)
 		members_before = members_now
 	elif len(members_now) > len(members_before):
-		change_members = isIn(group)
-		sendMsg(change_members)
+		change_members = isIn(members_before, members_now)
+		sendInMsg(change_members, bot)
 		members_before = members_now
 
