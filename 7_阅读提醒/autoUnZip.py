9ef7f2b3b5249cd4f28cda2c4a60cb0bd4287a3e
CoolCoolBo <512167784@qq.com>
2018-11-07 00:13:16 +0800

    面向对象的自动解压

---
+++ 7_阅读提醒/autoUnZip.py
@@ -0,0 +1,62 @@
+import os 
+import shutil
+
+"""
+自动扫描压缩包并解压，然后删除压缩包
+
+"""
+
+class ZipManager:
+
+		def __init__(self, file_path):
+			self.file_path = file_path 
+
+		def scanf(self):
+			files = os.listdir(self.file_path)
+			for f in files:
+				if f.endswith('zip'):
+					return f
+
+		def un_zip(self, f):
+			path = f.split('.')[0]
+			filePath = self.file_path + '/' + f   
+			target_path = self.file_path + '/' + path
+			# os.mkdir(target_path)
+			shutil.unpack_archive(filePath, target_path)
+
+		def delete(self, f):
+			filePath = self.file_path + '/' + f   
+			os.remove(filePath)
+
+		def move(self, f):
+			# os.mkdir('/home/bo/Downloads/packages')
+			file = '/home/bo/Downloads/' + f
+			shutil.move(file, '/home/bo/Downloads/packages')
+
+
+		def run(self):
+			if self.file_path == '/home/bo/Desktop':
+				f = self.scanf()
+				self.un_zip(f)
+				self.delete(f)
+			if self.file_path == '/home/bo/Downloads':
+				f = self.scanf()
+				self.un_zip(f)
+				self.move(f)
+
+
+
+
+
+
+# while True:
+# 	f = scanf()
+# 	if f:
+# 		un_zip(f)
+# 		delete(f)
+manager1 = ZipManager('/home/bo/Desktop')
+manager2 = ZipManager('/home/bo/Downloads')
+print(manager1)
+print(manager2)
+manager1.run()
+manager2.run()
