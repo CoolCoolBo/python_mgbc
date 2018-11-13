14a70f44a880500eabe3aeaa008bdae36591ea2e
CoolCoolBo <512167784@qq.com>
2018-11-14 00:15:34 +0800

    项目更新监测

--- python网络与爬虫/1.github项目更新监测并打开网页/1.py
+++ python网络与爬虫/1.github项目更新监测并打开网页/1.py
@@ -3,32 +3,27 @@ import webbrowser
 import time
 
 
-def get_curList(api_url):
-    starred_info = requests.get(api_url).json()
-    print(starred_info)
-    for each in starred_info:
-        cur_list.append(each['id'])
-    return cur_list
 
 
-def open_newRepo(cur_list, last_list):
-    for cur in cur_list:
-        if cur not in last_list:
-            owner = cur['owner']['login']
-            repo_name = cur['full_name']
-            url = "https://github.com/" + owner + '/' + repo_name
-            print(url)
-            # webbrowser.open('www.baidu.com')
 
-last_list = None
-cur_list = []
+starred_list = []
+starred_info = requests.get('https://api.github.com/users/CoolCoolBo/starred').json()
+for e in starred_info:
+    starred_list.append(e['id'])
 while True:
-    cur_list = get_curList('https://api.github.com/users/kennethreitz/starred')
-    print('正在监测中')
-    print('The cur_list is: ', cur_list)
-    if not last_list:
-        last_list = cur_list
-    # open_newRepo(cur_list, last_list)
-    last_list = cur_list
-    print('The last_list is: ', last_list)
+    print('正在监测...')
+    cur_starred = requests.get('https://api.github.com/users/CoolCoolBo/starred').json()
+    for each in cur_starred:
+        if each['id'] not in starred_list:
+            starred_list.append(each['id']) 
+            owner = each['owner']['login']
+            repo_name = each['full_name']
+            url = "https://github.com/" + repo_name
+            print(url)
+            webbrowser.open(url)
     time.sleep(10)
+            # owner = each['owner']['login']
+            # repo_name = each['full_name']
+            # url = "https://github.com/" + owner + '/' + repo_name
+            # print(url)
+            # webbrowser.open(url)
