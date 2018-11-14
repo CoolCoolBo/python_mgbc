988aba48948f3bf7f1032d7d38c240adf488bc67
CoolCoolBo <512167784@qq.com>
2018-11-14 09:45:56 +0800

    1_github项目更新监测/1.py

--- python网络与爬虫/１_github项目更新监测/1.py
+++ python网络与爬虫/１_github项目更新监测/1.py
@@ -3,9 +3,6 @@ import webbrowser
 import time
 
 
-
-
-
 starred_list = []
 starred_info = requests.get('https://api.github.com/users/CoolCoolBo/starred').json()
 for e in starred_info:
@@ -22,8 +19,3 @@ while True:
             print(url)
             webbrowser.open(url)
     time.sleep(10)
-            # owner = each['owner']['login']
-            # repo_name = each['full_name']
-            # url = "https://github.com/" + owner + '/' + repo_name
-            # print(url)
-            # webbrowser.open(url)
